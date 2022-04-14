2bd5413b99fa29add27239f4f3f27034c68c1999
kishangohel18 <97102699+kishangohel18@users.noreply.github.com>
2022-04-14 10:11:41 +0530

    user created in database

---
+++ API/GiellyGreenApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GiellyGreenApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ API/GiellyGreenApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GiellyGreenApi.WebApiApplication" Language="C#" %>
