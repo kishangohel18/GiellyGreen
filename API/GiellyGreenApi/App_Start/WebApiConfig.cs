1be473846920519caddfdc3bf9aa1b25e7a6bb08
kishangohel18 <97102699+kishangohel18@users.noreply.github.com>
2022-04-14 13:41:42 +0530

    prelunch commit

--- API/GiellyGreenApi/App_Start/WebApiConfig.cs
+++ API/GiellyGreenApi/App_Start/WebApiConfig.cs
@@ -6,16 +6,19 @@ using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
+
 namespace GiellyGreenApi
 {
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- API/GiellyGreenApi/Controllers/LoginController.cs
+++ API/GiellyGreenApi/Controllers/LoginController.cs
@@ -6,25 +6,58 @@ using System.Linq;
 using System.Net;
 using System.Configuration;
 using System.Web.Http;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Web.Http.Cors;
+
 
 namespace GiellyGreenApi.Controllers
 {
+    [EnableCors(origins: "http://3fb2-106-201-236-89.ngrok.io/", headers: "*", methods: "*")]
+
     public class LoginController : ApiController
     {
         public JsonResponse LoginPost(LoginModel model)
         {
-            var ObjResponse = new JsonResponse();
+                var ObjResponse = new JsonResponse();
             try
-            {
+            {                
                 if (ModelState.IsValid)
                 {
                     if (model.email == ConfigurationManager.AppSettings["email"].ToString() && model.password == ConfigurationManager.AppSettings["password"].ToString())
                     {
+                        //var accessToken = string.Empty;
+                        //var keyValues = new List<KeyValuePair<string, string>>
+                        //{
+                        //    new KeyValuePair<string, string>("email",model.email),
+                        //    new KeyValuePair<string, string>("password",model.password),
+                        //    new KeyValuePair<string, string>("grant_type","password")
+                        //};
+
+                        //var request = new HttpRequestMessage(HttpMethod.Post, Url + "Token");
+                        //request.Content = new FormUrlEncodedContent(keyValues);
+
+
+                        //var client = new HttpClient();
+                        //var response = client.SendAsync(request).Result;
+
+                        //using (HttpContent content = response.Content)
+                        //{
+                        //    var json = content.ReadAsStringAsync();
+                        //    JObject jwtDynamic = JsonConvert.DeserializeObject<dynamic>(json.Result);
+                        //    var accessTokenExpiration = jwtDynamic.Value<DateTime>(".expires");
+                        //    accessToken = jwtDynamic.Value<string>("access_token");
+                        //    var Username = jwtDynamic.Value<string>("username");
+                        //    var AccessTokenExpirationDate = accessTokenExpiration;
+
+                        //}
+
                         ObjResponse = JsonResponseHelper.JsonResponseMessage(1, "Success.", model);
                     }
                     else
                     {
-                        ObjResponse = JsonResponseHelper.JsonResponseMessage(1, "Fail.", null);
+                        ObjResponse = JsonResponseHelper.JsonResponseMessage(0, "Fail.", null);
                     }
                 }
                 else
