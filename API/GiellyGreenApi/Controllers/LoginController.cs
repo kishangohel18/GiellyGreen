187265431f5c0bd34577569b657078a1df6d7e1c
kishangohel18 <97102699+kishangohel18@users.noreply.github.com>
2022-04-14 11:30:25 +0530

    login success

---
+++ API/GiellyGreenApi/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using GiellyGreenApi.Helper;
+using GiellyGreenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Configuration;
+using System.Web.Http;
+
+namespace GiellyGreenApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public JsonResponse LoginPost(LoginModel model)
+        {
+            var ObjResponse = new JsonResponse();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    if (model.email == ConfigurationManager.AppSettings["email"].ToString() && model.password == ConfigurationManager.AppSettings["password"].ToString())
+                    {
+                        ObjResponse = JsonResponseHelper.JsonResponseMessage(1, "Success.", model);
+                    }
+                    else
+                    {
+                        ObjResponse = JsonResponseHelper.JsonResponseMessage(1, "Fail.", null);
+                    }
+                }
+                else
+                {
+                    var allErrors = ModelState.Values.SelectMany(x => x.Errors);
+                    ObjResponse = JsonResponseHelper.JsonResponseMessage(0, "Error.", allErrors);
+                }
+            }
+            catch (Exception ex)
+            {
+                ObjResponse = JsonResponseHelper.JsonResponseMessage(2, ex.Message, null);
+            }
+
+            return ObjResponse;
+        }
+    }
+}
---
+++ API/GiellyGreenApi/Models/JsonResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiellyGreenApi.Models
+{
+    public class JsonResponse
+    {
+        public int ResponseStatus { get; set; }
+        public string Message { get; set; }
+        public dynamic Result { get; set; }
+
+    }
+}
