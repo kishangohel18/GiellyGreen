ce03460511de991b1483db539dc74fcab8d40e27
Preksha-Thakkar <97083910+Preksha-Thakkar@users.noreply.github.com>
2022-04-25 20:15:38 +0530

    commit

---
+++ API/GiellyGreenApi/Models/UpdateActiveStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiellyGreenApi.Models
+{
+    public class UpdateActiveStatus
+    {
+        public int SupplierId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
